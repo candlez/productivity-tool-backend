c78b0458cf18351d18c0ffbe521cd778d4dfe5f0
candlez <thomasmcnamara@vt.edu>
2025-05-16 18:25:27 -0400

    added db connection

---
+++ db.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2';
+
+export const db = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import { app } from "./app.js";
+import { db } from "./db.js";
 
 let server;
 
 const startServer = async () => {
+    try {
+        const connection = await db.getConnection();
+        console.log("Connected to database");
+        connection.release();
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        process.exit(1);
+    }
 
     server = app.listen(process.env.NODE_PORT || 9000, () => {
         console.log(`Servers is listening on port ${process.env.NODE_PORT || 9000}...`);
