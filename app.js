6a73cd208906767b85ff10271ba6616e6b7d0f8f
candlez <thomasmcnamara@vt.edu>
2025-05-16 19:05:57 -0400

    added test routes

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import { testRouter } from './routes/test';
 
 export const app = express();
+
+app.use(testRouter);
 
---
+++ routes/test.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { db } from "../db";
+
+export const testRouter = Router();
+
+testRouter.get("/api/test1", async (req, res) => {
+    return res.send("here is the test");
+});
+
+testRouter.get("api/test2", async (req, res) => {
+    let connection;
+
+    try {
+        connection = await db.getConnection();
+
+        const [results] = await connection.query(
+            "SELECT * FROM test;"
+        );
+
+        return res.send(results);
+    } catch (error) {
+ 
+    } finally {
+        if (connection) connection.release();
+    }
+});
