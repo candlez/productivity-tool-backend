2e77b104316bb69cbf3b27920608cef17d31051f
Thomas McNamara <thomasmcnamara@vt.edu>
2025-11-22 10:37:30 -0500

    feat: [MVP-2] added final pillar routes

--- src/routes/pillar.routes.ts
+++ src/routes/pillar.routes.ts
@@ -5,11 +5,13 @@ import { parseToken } from "../middleware/auth.mid.js";
 import { PillarRepository } from "../repositories/pillar.repo.js";
 import { PillarService } from "../services/pillar.service.js";
 import type { InputPillar, Pillar, PublicPillar } from "../types/pillar.types.js";
-import { sendArray, sendCreated } from "../util/rest.util.js";
+import { sendArray, sendCreated, sendDeleted, sendOneItem } from "../util/rest.util.js";
 import { ContextService } from "../services/context.service.js";
 import { IDService } from "../services/id.service.js";
 import { inputPillarSchema } from "../joi/pillar.schema.js";
 import { JoiValidationError } from "../types/error.types.js";
+import Joi from "joi";
+import type { UUID } from "crypto";
 
 
 export const pillarRouter: Router = Router();
@@ -23,7 +25,7 @@ const pillarService = new PillarService(pillarRepository, idService);
 
 
 pillarRouter.get("/", async (req, res) => {
-    const pillars: Pillar[] = await pillarService.getPillarsByUser(ContextService.getCallingUser()!.id);
+    const pillars: Pillar[] = await pillarService.getPillars();
     const publicPillars: PublicPillar[] = pillars; // no need to map as there is no difference
     return sendArray<PublicPillar>(res, publicPillars);
 });
@@ -42,4 +44,53 @@ pillarRouter.post("/", async (req, res) => {
     const pillar: Pillar = await pillarService.createPillar(validation.value);
     const publicPillar: PublicPillar = pillar;
     return sendCreated(res, publicPillar, publicPillar.id);
+});
+
+
+const pathParamSchema = Joi.object<{ pillarID: UUID }>({
+    pillarID: Joi.string().uuid().required()
+}).unknown(false);
+
+
+pillarRouter.get("/:pillarID", async (req, res) => {
+    const validation: ValidationResult<{ pillarID: UUID }> = pathParamSchema.validate(req.params);
+
+    if (validation.error) {
+        throw new JoiValidationError("Server encountered invalid data in the request parameters", validation.error.details);
+    }
+
+    const pillar: Pillar = await pillarService.getPillarByID(validation.value.pillarID);
+    const publicPillar: PublicPillar = pillar;
+    return sendOneItem(res, publicPillar, publicPillar.id);
+});
+
+
+pillarRouter.put("/:pillarID", async (req, res) => {
+    const paramValidation: ValidationResult<{ pillarID: UUID }> = pathParamSchema.validate(req.params);
+
+    if (paramValidation.error) {
+        throw new JoiValidationError("Server encountered invalid data in the request parameters", paramValidation.error.details);
+    }
+
+    const bodyValidation: ValidationResult<InputPillar> = inputPillarSchema.validate(req.body);
+
+    if (bodyValidation.error) {
+        throw new JoiValidationError("Server encountered invalid data in the request body", bodyValidation.error.details);
+    }
+
+    const updatedPillar: Pillar = await pillarService.updatePillar(paramValidation.value.pillarID, bodyValidation.value);
+    const publicPillar: PublicPillar = updatedPillar;
+    return sendOneItem(res, publicPillar, publicPillar.id);
+});
+
+
+pillarRouter.delete("/:pillarID", async (req, res) => {
+    const validation: ValidationResult<{ pillarID: UUID }> = pathParamSchema.validate(req.params);
+
+    if (validation.error) {
+        throw new JoiValidationError("Server encountered invalid data in the request parameters", validation.error.details);
+    }
+
+    await pillarService.deletePillar(validation.value.pillarID);
+    return sendDeleted(res);
 });
--- src/services/pillar.service.ts
+++ src/services/pillar.service.ts
@@ -2,22 +2,36 @@ import type { UUID } from "crypto";
 
 import type { PillarRepository } from "../repositories/pillar.repo.js";
 import type { InputPillar, Pillar } from "../types/pillar.types.js";
-import { Predicate } from "../util/predicate.util.js";
+import { UpdatePredicate, WherePredicate } from "../util/predicate.util.js";
 import type { IDService } from "./id.service.js";
 import { uuidToBuffer } from "../util/uuid.util.js";
+import { NotFoundError } from "../types/error.types.js";
+import { ContextService } from "./context.service.js";
+import type { PublicUser } from "../types/user.types.js";
 
 
 
 export class PillarService {
     constructor(private pillarRepository: PillarRepository, private idServices: IDService) {}
 
-    public async getPillarsByUser(userID: UUID): Promise<Pillar[]> {
+    public async getPillars(): Promise<Pillar[]> {
         
-        const predicate: Predicate = new Predicate();
-        predicate.equalTo("user_id", uuidToBuffer(userID));
+        const user: PublicUser = ContextService.verifyCallingUser();
+        const predicate: WherePredicate = new WherePredicate();
+        predicate.equalTo("user_id", uuidToBuffer(user.id));
         const pillars = await this.pillarRepository.getPillars(predicate);
         return pillars;
     }
+
+    public async getPillarByID(pillarID: UUID): Promise<Pillar> {
+
+        const user: PublicUser = ContextService.verifyCallingUser();
+        const predicate: WherePredicate = new WherePredicate();
+        predicate.equalTo("pillar_id", uuidToBuffer(pillarID));
+        predicate.equalTo("user_id", uuidToBuffer(user.id));
+        return await this.pillarRepository.getOnePillar(predicate);
+    }
+
 
     public async createPillar(inputPillar: InputPillar): Promise<Pillar> {
 
@@ -38,4 +52,23 @@ export class PillarService {
     }
 
 
+    public async updatePillar(pillarID: UUID, inputPillar: InputPillar): Promise<Pillar> {
+
+        const predicate = new UpdatePredicate();
+        predicate.equalTo("user_id", inputPillar.userID);
+        predicate.equalTo("name", inputPillar.name);
+        predicate.equalTo("theme_id", inputPillar.themeID);
+        predicate.equalTo("description", inputPillar.description);
+        predicate.equalTo("max_score", inputPillar.maxScore);
+        predicate.equalTo("active", inputPillar.active);
+        await this.pillarRepository.updatePillar(pillarID, predicate)
+        const pillar: Pillar = await this.getPillarByID(pillarID);
+        return pillar;
+    }
+
+
+    public async deletePillar(pillarID: UUID): Promise<void> {
+
+        await this.pillarRepository.deletePillar(pillarID);
+    }
 }
